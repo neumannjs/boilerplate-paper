55eeaf8002db18f397ce17ffc51d85a32651ed69
Gijs van Dam <gvandam@gmail.com>
2020-03-10 15:12:22 +0800

    Add task paper to DOCS, with docx reference file

--- tasks/build.ps1
+++ tasks/build.ps1
@@ -1,10 +1,28 @@
-param ($source, $defaults)
+param ($source, $defaults, $referencedoc, $to, $output)
 
 $Dir = Get-ChildItem -Path "$source".replace("\","/")
 $defaults = $defaults.replace("\","/")
  
 $List = $Dir | Where-Object {$_.extension -eq ".md"}| ForEach-Object {$_.FullName} 
 $List = $([string]$List).replace("\","/")
-Write-Host("pandoc $List -defaults=$defaults")
 
-Invoke-Expression "pandoc $List --defaults=$defaults"
+$Command = "pandoc $List --defaults=$defaults"
+
+if($referencedoc){
+  $referencedoc =$referencedoc.replace("\","/")
+  $Command = "$Command --reference-doc=$referencedoc"
+}
+
+if($to){
+  $to =$to.replace("\","/")
+  $Command = "$Command --to=$to"
+}
+
+if($output){
+  $output =$output.replace("\","/")
+  $Command = "$Command --output=$output"
+}
+
+Write-Host($Command)
+
+Invoke-Expression $Command
