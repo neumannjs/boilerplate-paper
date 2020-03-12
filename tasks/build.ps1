d82ba78f6167518bf75d7e60622d845b7110d747
Gijs van Dam <gvandam@gmail.com>
2020-03-12 14:38:33 +0800

    Haul build code into function

--- tasks/build.ps1
+++ tasks/build.ps1
@@ -1,32 +1,37 @@
 param ($source, $defaults, $referencedoc, $to, $output, $commit)
 
-if($commit){
+if ($commit) {
   Invoke-Expression "git checkout $commit"
 }
 
-$Dir = Get-ChildItem -Path "$source".replace("\","/")
-$defaults = $defaults.replace("\","/")
+build($source, $defaults, $referencedoc, $to, $output)
+
+function build($source, $defaults, $referencedoc, $to, $output) {
+  $Dir = Get-ChildItem -Path "$source".replace("\", "/")
+  $defaults = $defaults.replace("\", "/")
  
-$List = $Dir | Where-Object {$_.extension -eq ".md"}| ForEach-Object {$_.FullName} 
-$List = $([string]$List).replace("\","/")
+  $List = $Dir | Where-Object { $_.extension -eq ".md" } | ForEach-Object { $_.FullName } 
+  $List = $([string]$List).replace("\", "/")
 
-$Command = "pandoc $List --defaults=$defaults"
+  $Command = "pandoc $List --defaults=$defaults"
 
-if($referencedoc){
-  $referencedoc =$referencedoc.replace("\","/")
-  $Command = "$Command --reference-doc=$referencedoc"
+  if ($referencedoc) {
+    $referencedoc = $referencedoc.replace("\", "/")
+    $Command = "$Command --reference-doc=$referencedoc"
+  }
+
+  if ($to) {
+    $to = $to.replace("\", "/")
+    $Command = "$Command --to=$to"
+  }
+
+  if ($output) {
+    $output = $output.replace("\", "/")
+    $Command = "$Command --output=$output"
+  }
+
+  Write-Host($Command)
+
+  Invoke-Expression $Command
+
 }
-
-if($to){
-  $to =$to.replace("\","/")
-  $Command = "$Command --to=$to"
-}
-
-if($output){
-  $output =$output.replace("\","/")
-  $Command = "$Command --output=$output"
-}
-
-Write-Host($Command)
-
-Invoke-Expression $Command
