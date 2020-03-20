85061cd79c0af1d95c5b2fdc88be026183caf3e5
Gijs van Dam <gvandam@gmail.com>
2020-03-20 21:20:59 +0800

    Fix tmpdir

--- style/tikz.lua
+++ style/tikz.lua
@@ -2,8 +2,11 @@ local function starts_with(start, str) return str:sub(1, #start) == start end
 
 local function tikz2image(src, filetype, outfile)
     local tmp = os.tmpname()
+    local tmpdir = "./output"
     if not starts_with("/tmp/", tmp) then
         tmp = "./output" .. tmp
+    else
+        tmpdir = "/tmp/"
     end
     local f, err = io.open(tmp .. ".tex", 'w')
     if f==nil then
@@ -15,7 +18,7 @@ local function tikz2image(src, filetype, outfile)
     f:write(src)
     f:write("\n\\end{document}\n")
     f:close()
-    os.execute("pdflatex -output-directory " .. ".\\output " .. tmp)
+    os.execute("pdflatex -output-directory " .. tmpdir  .. " " .. tmp)
     if filetype == 'pdf' then
         os.rename(tmp .. ".pdf", outfile)
     elseif filetype == 'png' then
